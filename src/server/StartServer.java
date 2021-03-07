99ac615de95b7faeb61ba106c8f5ead77512ef3a
Christopher <77005138+krilleellerstoffe@users.noreply.github.com>
2021-03-07 15:20:35 +0100

    Skapade LogFileManager och Log klasserna. Låg till lite test kod i startserver

--- src/server/StartServer.java
+++ src/server/StartServer.java
@@ -1,9 +1,13 @@
 package server;
 
-import client.MessageClient;
-
 public class StartServer {
     public static void main(String[] args) {
         MessageServer messageServer = new MessageServer(new MessageManager(), 2555);
+        LogFileManager lfm = new LogFileManager("files/log.dat");
+        lfm.addLog("A log");
+        System.out.println(System.currentTimeMillis());
+        for (Log log: lfm.readLogFile(System.currentTimeMillis()-10000l, System.currentTimeMillis())) { //tests for logs made within last 10 seconds
+            System.out.println(log);
+        }
     }
 }
